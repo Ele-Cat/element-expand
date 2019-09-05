8c08495865ce387403063feb5432246af3f3078c
lijiamao <lijiamao@itimebox.com>
2019-09-05 18:18:08 +0800

    完善代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <el-row>
       <el-col :span="12">
-        <p style="margin-top:20px;">table-expand-example</p>
+        <p style="margin-top:20px;">Table Expand Example</p>
         <el-table ref="table" size="small" :data="tableData" stripe border style="width: 800px;margin:20px auto;">
           <el-table-column label="商品 ID" prop="id.value"></el-table-column>
           <el-table-column label="商品名称" prop="name.value"></el-table-column>
@@ -16,10 +16,10 @@
             <template slot-scope="props">
               <el-form label-position="left" inline class="demo-table-expand">
                 <el-form-item :label="item.key" v-for="(item, index) in props.row" :key="item.value" :index="index">
-                  <el-popover v-if="item.key != '商品 ID'" placement="top" width="240" v-model="item.visible">
+                  <el-popover v-if="item.key != '商品 ID'" placement="top" width="240" :value="item.visible">
                     <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 4}" placeholder="请输入内容" v-model="popoverInput"></el-input>
                     <div style="text-align: right; margin: 10px 0 0">
-                      <el-button size="mini" type="text" @click="item.visible = false">取消</el-button>
+                      <el-button size="mini" type="text" @click="clickDom()">取消</el-button>
                       <el-button type="primary" size="mini" @click="confirmChange(item)">确定</el-button>
                     </div>
                     <span style="cursor:pointer;" slot="reference" @click="showPopover(item)">{{ item.value }}</span>
@@ -32,8 +32,8 @@
         </el-table>
       </el-col>
       <el-col :span="12">
-        <p style="margin-top:20px;">data contrast</p>
-        <p v-for="(item, index) in tableData" :key="item.id" :index="index">
+        <p style="margin-top:20px;">Data Contrast</p>
+        <p v-for="(item, index) in tableData" :key="item.id.value" :index="index">
           {{item.id.value}}
           {{item.name.value}}
           {{item.shop.value}}
@@ -205,11 +205,15 @@ export default {
     }
   },
   created() {
+    let tableData = this.tableData
+    tableData.map(item => {
+      item.expansion = false
+    })
   },
   methods: {
     toogleExpand(row) {
       let $table = this.$refs.table;
-      this.tableData.map((item, index) => {
+      this.tableData.map(item => {
         if (row.id != item.id) {
           $table.toggleRowExpansion(item, false)
           item.expansion = false
@@ -219,6 +223,9 @@ export default {
       })
       $table.toggleRowExpansion(row)
     },
+    clickDom() {
+      document.querySelector("#app").click();
+    },
     showPopover(item) {
       console.log(item)
       this.popoverInput = item.value
@@ -226,6 +233,7 @@ export default {
     confirmChange(item) {
       console.log(item)
       item.value = this.popoverInput
+      this.clickDom()
     }
   }
 }
